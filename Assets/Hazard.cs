f5b40d379bd922be650c151cab86b7674653d3fd
Ryan Massia [Student] <RMassia03@student.sl.on.ca>
2019-11-23 10:59:34 -0500

    test

---
+++ Assets/Hazard.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+    public GameObject Spawn;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Floor")
+            transform.position = new Vector3(0, 0, 0);
+    }
+
+    void Respawn()
+    {
+        transform.position = Spawn.transform.position;
+    }
+
+}
