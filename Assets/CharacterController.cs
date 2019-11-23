48bcf8aed3b47dbd5dc9cc02c27259dbe06167e0
Nicholas Harrison [Student] <nicholas.harrison@student.sl.on.ca>
2019-11-22 19:53:13 -0500

    start on charater controller

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private CharacterController characterController;
+    public float moveSpeed = 10.0f;
+    GameObject player;
+    void Start()
+    {
+       
+    }
+
+   
+    void Update()
+    {
+        Look();
+    }
+
+    public void Look()
+    {
+        Vector3 move= new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        
+    }
+}
